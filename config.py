bcb8f814166e2b72bd3595e17138493b062b6de7
wouterstultiens <wouterstultiens@gmail.com>
2025-06-04 22:07:21 +0200

    gcp setup 2

--- config.py
+++ config.py
@@ -73,6 +73,27 @@ PRACTICES = [
         "selector": "div#vacature-overzichtje",
         "get_full_html": False
     },
+]
+
+PRACTICES2 = [
+    {
+        "name": "De Deventer Tandartspraktijk",
+        "url": "https://www.dedeventertandartspraktijk.nl/vacatures",
+        "selector": "article#post-131",
+        "get_full_html": False
+    },
+    {
+        "name": "Dental Clinics Colmschate",
+        "url": "https://www.werkenbijdentalclinics.nl/vacatures/?_locatie=Colmschate",
+        "selector": "div.section-element.section-element-vacancies-archive.\@container\/element-vacancies-archive",
+        "get_full_html": False
+    },
+    {
+        "name": "De Watersnip",
+        "url": "https://dewatersnip.com/vacatures",
+        "selector": "div#vacature-overzichtje",
+        "get_full_html": False
+    },
     {
         "name": "Tandartsen Centrum Deventer",
         "url": "https://www.stcdeventer.nl/vacatures",
